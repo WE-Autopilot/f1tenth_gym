7ce52ca660f121009aed18acf6bcdaf6956bcc09
Hamza Elkababji <hamza.kababji@gmail.com>
2025-04-14 13:24:15 -0400

    Made setuptools automatically find the packages under weap_util

--- weap_util/setup.py
+++ weap_util/setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='weap_util',
     version='0.1.0',
     author='Aly Ashour',
-    package_dir={'': '.'},
-    install_requires=['numpy<=1.22.0,>=1.18.0', 'opencv-python-headless==4.11.0.86']
+    packages=find_packages(),  # This will automatically find the "weap_util" package inside
+    install_requires=[
+        'numpy>=1.18.0,<=1.22.0',
+        'opencv-python-headless==4.11.0.86'
+    ]
 )
